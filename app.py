5a1b5fe429757ab9fcd851148a5f8daf5f3d1855
Ridoh7 <ridohlawal96@gmail.com>
2024-12-04 14:41:49 +0100

    Conditional statement

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print("Hello world!")
+
+age=int(input("Enter your age: "))
+if age>=50:
+    print("You are a citizen")
+elif age==18:
+    print("You are an adult")
+else:
+    print("You are an underage")
